d748790ca01905e43822addc8b5d97ead572fc27
uncertain-factors <2319178705@qq.com>
2024-04-29 00:06:35 +0800

    sampler

--- datasets/sampler.py
+++ datasets/sampler.py
@@ -33,8 +33,10 @@ class RandomIdentitySampler(Sampler):
         for pid in self.pids:
             idxs = self.index_dic[pid]
             num = len(idxs)
+            # 确保每个id至少有num_instances个样本
             if num < self.num_instances:
                 num = self.num_instances
+            # 计算所有id的样本总和
             self.length += num - num % self.num_instances
 
     def __iter__(self):
@@ -42,10 +44,12 @@ class RandomIdentitySampler(Sampler):
 
         for pid in self.pids:
             idxs = copy.deepcopy(self.index_dic[pid])
+            # 当id的对应样本索引个数不足num_instance时，随机复制索引并打乱
             if len(idxs) < self.num_instances:
                 idxs = np.random.choice(idxs, size=self.num_instances, replace=True)
             random.shuffle(idxs)
             batch_idxs = []
+            # 遍历每个id的索引，成批加入batch_idxs_dict字典中（多余的丢掉），键为id，值为一批索引
             for idx in idxs:
                 batch_idxs.append(idx)
                 if len(batch_idxs) == self.num_instances:
@@ -54,7 +58,7 @@ class RandomIdentitySampler(Sampler):
 
         avai_pids = copy.deepcopy(self.pids)
         final_idxs = []
-
+        # 随机抽出k个id，每个id抽出一批索引加入final_idxs，并标记这些id
         while len(avai_pids) >= self.num_pids_per_batch:
             selected_pids = random.sample(avai_pids, self.num_pids_per_batch)
             for pid in selected_pids:
